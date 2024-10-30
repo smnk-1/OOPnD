9b3ba463bb6289b638c14c6ab881965b36d3092c
smnk-mcb <semikinnikolay1@gmail.com>
2024-10-30 16:58:39 +0600

    feat: add move command

--- StarWars.Lib/MoveClass.cs
+++ StarWars.Lib/MoveClass.cs
@@ -1,11 +1,22 @@
 namespace StarWars.Lib;
 
-public interface IMovable
+public interface IMoving
 {
-    
+    int[] Position { get; set; }
+    int[] Velocity { get; }
 }
 
-public class MoveCommand: ICommand
+public class MoveCommand : ICommand
 {
-    
+    private IMoving obj;
+
+    public MoveCommand(IMoving obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Position = obj.Position.Select((value, index) => value + obj.Velocity[index]).ToArray();
+    }
 }
