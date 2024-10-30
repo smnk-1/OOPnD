e7f903e47f5f4e3d96ced9d36805ef810a16f658
smnk-mcb <semikinnikolay1@gmail.com>
2024-10-30 16:59:46 +0600

    chore: create a file for move command tests

--- StarWars.Tests/MoveCommandTests.cs
+++ StarWars.Tests/MoveCommandTests.cs
@@ -1,10 +1,23 @@
-namespace StarWars.Tests;
+using Moq;
+using StarWars.Lib;
 
+namespace StarWars.Test;
+
+// dotnet test в каталоге star-wars чтобы запустить тесты
 public class MoveCommandTests
 {
     [Fact]
     public void MoveCommandPos()
-    {
+       {
+        var moving = new Mock<IMoving>();
 
+        moving.SetupGet(m => m.Position).Returns(new int[] { 12, 5 });
+        moving.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 });
+
+        var cmd = new MoveCommand(moving.Object);
+        cmd.Execute();
+
+        moving.VerifySet(m => m.Position = new int[] { 5, 8 });
     }
+
 }
