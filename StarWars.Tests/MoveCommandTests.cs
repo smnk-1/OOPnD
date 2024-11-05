d3bbb1d909df227a24aaf6b742dcc7f2754946d7
smnk-mcb <semikinnikolay1@gmail.com>
2024-11-05 17:15:38 +0600

    chore: add tests for move command

--- StarWars.Tests/MoveCommandTests.cs
+++ StarWars.Tests/MoveCommandTests.cs
@@ -7,7 +7,7 @@ namespace StarWars.Test;
 public class MoveCommandTests
 {
     [Fact]
-    public void MoveCommandPos()
+    public void MoveCommandPositionUpdateTest()
        {
         var moving = new Mock<IMoving>();
 
@@ -20,4 +20,43 @@ public class MoveCommandTests
         moving.VerifySet(m => m.Position = new int[] { 5, 8 });
     }
 
+    [Fact]
+    public void MoveCommandPositionIsNotReadableTest()
+    {
+        var movingObject = new Mock<IMoving>();
+
+        movingObject.SetupGet(m => m.Position).Throws(new InvalidOperationException());
+        movingObject.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 });
+        
+        var cmd = new MoveCommand(movingObject.Object);
+        
+        Assert.Throws<InvalidOperationException>(() => cmd.Execute());
+    }
+
+    [Fact]
+    public void MoveCommandVelocityIsNotReadableTest()
+    {
+        var movingObject = new Mock<IMoving>();
+
+        movingObject.SetupGet(m => m.Position).Returns(new int[] { 12, 5 });
+        movingObject.SetupGet(m => m.Velocity).Throws(new InvalidOperationException());
+        
+        var cmd = new MoveCommand(movingObject.Object);
+        
+        Assert.Throws<InvalidOperationException>(() => cmd.Execute());
+    }
+
+    [Fact]
+    public void MoveCommandPositionIsNotWritableTest()
+    {
+        var movingObject = new Mock<IMoving>();
+
+        movingObject.SetupGet(m => m.Position).Returns(new int[] { 12, 5 });;
+        movingObject.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 });
+        movingObject.SetupSet(m => m.Position = It.IsAny<int[]>()).Throws(new InvalidOperationException());
+
+        var cmd = new MoveCommand(movingObject.Object);
+
+        Assert.Throws<InvalidOperationException>(() => cmd.Execute());
+    }
 }
