0171a665fd73d1307ec27b75993ad4896650fdbb
terracodum <nikitakharlov007@gmail.com>
2024-11-05 16:02:28 +0600

    feat: add rotate command and tests

---
+++ StarWars.Lib/RotateClass.cs
@@ -0,0 +1,23 @@
+﻿namespace StarWars.Lib;
+
+public interface IRotating
+{
+    int Angle { get; set; }
+    int RotateVelocity { get; }
+}
+
+public class RotateCommand : ICommand
+{
+    private readonly IRotating obj;
+
+    public RotateCommand(IRotating obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Angle += obj.RotateVelocity;
+        obj.Angle = obj.Angle % 360;
+    }
+}
---
+++ StarWars.Tests/RotateCommandTest.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using StarWars.Lib;
+
+namespace StarWars.Test;
+public class RotateCommandTests
+{
+    [Fact]
+    public void Test45plus90()
+    {
+        var rotating = new Mock<IRotating>();
+        rotating.SetupGet(r => r.Angle)
+                .Returns(() => 45);
+        rotating.SetupGet(r => r.RotateVelocity)
+                .Returns(() => 90);
+
+        var cmd = new RotateCommand(rotating.Object);
+        cmd.Execute();
+
+        rotating.VerifySet(r => r.Angle = 135);
+    }
+
+    [Fact]
+    public void TestCannotReadAngle()
+    {
+        var rotating = new Mock<IRotating>();
+        rotating.SetupGet(m => m.Angle).Throws(new Exception("Cannot read angle"));
+        rotating.SetupGet(m => m.RotateVelocity).Returns(() => 90);
+        ICommand rotate = new RotateCommand(rotating.Object);
+
+        Assert.Throws<Exception>(rotate.Execute);
+    }
+
+    [Fact]
+    public void TestCannotReadRotateVelocity()
+    {
+        var rotating = new Mock<IRotating>();
+        rotating.SetupGet(m => m.Angle).Returns(() => 45);
+        rotating.SetupGet(m => m.RotateVelocity).Throws(new Exception("Cannot read angle"));
+        ICommand rotate = new RotateCommand(rotating.Object);
+
+        Assert.Throws<Exception>(rotate.Execute);
+    }
+
+    [Fact]
+    public void TestCannotSetAngle()
+    {
+        var rotating = new Mock<IRotating>();
+        rotating.SetupGet(m => m.Angle).Returns(() => 45);
+        rotating.SetupGet(m => m.RotateVelocity).Returns(() => 90);
+        rotating.SetupSet(m => m.Angle = It.IsAny<int>()).Throws(new Exception("Cannot set angle"));
+        ICommand rotate = new RotateCommand(rotating.Object);
+
+        Assert.Throws<Exception>(rotate.Execute);
+    }
+}
