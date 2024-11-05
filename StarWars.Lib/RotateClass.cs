4102a5683928f6738abbf0d0348072e71ec9a57c
terracodum <nikitakharlov007@gmail.com>
2024-11-05 16:02:28 +0600

    feat: add rotate command and tests

---
+++ StarWars.Lib/RotateClass.cs
@@ -0,0 +1,23 @@
+namespace StarWars.Lib;
+
+public interface IRotating
+{
+    int Angle   { get; set; }
+    int RotateVelocity { get; }
+}
+
+public class RotateCommand : ICommand
+{
+    private IRotating obj;
+
+    public RotateCommand(IRotating obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Angle += obj.RotateVelocity;
+        obj.Angle = obj.Angle % 360;
+    }
+}
