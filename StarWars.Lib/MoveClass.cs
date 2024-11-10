c9cff7c71117423bbee8d8beb32f79719f004344
Kreoniz <kreoniz@mail.ru>
2024-11-10 20:59:29 +0600

    feat: add queue-based class for long running operations

--- StarWars.Lib/MoveClass.cs
+++ StarWars.Lib/MoveClass.cs
@@ -1,22 +1,63 @@
-namespace StarWars.Lib;
-
-public interface IMoving
+namespace StarWars.Lib
 {
-    int[] Position { get; set; }
-    int[] Velocity { get; }
-}
-
-public class MoveCommand : ICommand
-{
-    private IMoving obj;
-
-    public MoveCommand(IMoving obj)
+    public interface IMoving
     {
-        this.obj = obj;
+        int[] Position { get; set; }
+        int[] Velocity { get; }
     }
 
-    public void Execute()
+    public class MoveCommand : ICommand
     {
-        obj.Position = obj.Position.Select((value, index) => value + obj.Velocity[index]).ToArray();
+        private readonly IMoving obj;
+
+        public MoveCommand(IMoving obj)
+        {
+            this.obj = obj;
+        }
+
+        public void Execute()
+        {
+            obj.Position = obj.Position.Select((value, index) => value + obj.Velocity[index]).ToArray();
+        }
+    }
+
+    public class CommandProcessor
+    {
+        private readonly Queue<ICommand> commandQueue = new Queue<ICommand>();
+        private readonly int interval;
+        private CancellationTokenSource? cts;
+
+        public CommandProcessor(int interval = 500)
+        {
+            this.interval = interval;
+        }
+
+        public void EnqueueCommand(ICommand command)
+        {
+            commandQueue.Enqueue(command);
+        }
+
+        public async Task ProcessCommandsAsync()
+        {
+            cts = new CancellationTokenSource();
+
+            while (commandQueue.Count > 0)
+            {
+                if (cts.Token.IsCancellationRequested)
+                    break;
+
+                var command = commandQueue.Dequeue();
+                command.Execute();
+
+                await Task.Delay(interval, cts.Token);
+            }
+        }
+
+        public void Cancel()
+        {
+            cts?.Cancel();
+            cts = null;
+        }
     }
 }
+
