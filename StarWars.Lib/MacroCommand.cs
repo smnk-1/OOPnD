0e92e737034cb2afa885927b4e49b0b72e4bad56
Kreoniz <kreoniz@mail.ru>
2024-12-15 20:59:57 +0600

    feat: add MacroCommand

---
+++ StarWars.Lib/MacroCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace StarWars.Lib;
+
+public class MacroCommand : ICommand
+{
+    private readonly ICommand[] cmds;
+    public MacroCommand(ICommand[] commands)
+    {
+        cmds = commands;
+    }
+
+    public void Execute()
+    {
+        var commandList = new List<ICommand>();
+        commandList = cmds.ToList<ICommand>();
+        commandList.ForEach(c => c.Execute());
+    }
+}
