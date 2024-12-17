e144f0231167c68bde286ac0a3d9753835ceb8d9
rabigede <108132920+rabigede@users.noreply.github.com>
2024-12-18 02:32:17 +0300

    Update InjectableTest.cs

--- StarWars.Tests/InjectableTest.cs
+++ StarWars.Tests/InjectableTest.cs
@@ -1,42 +1,36 @@
 ﻿using Moq;
+using NUnit.Framework;
 using StarWars.Lib;
+using System;
 
 namespace StarWars.Tests
 {
+    [TestFixture]
     public class CommandInjectableCommandTests
     {
+        [Test]
         public void Execute_InjectedCommand_ExecutesInjectedCommand()
         {
+            // Arrange
             var mockCommand = new Mock<ICommand>();
             var commandInjectableCommand = new CommandInjectableCommand();
             commandInjectableCommand.Inject(mockCommand.Object);
 
+            // Act
             commandInjectableCommand.Execute();
 
-            if (!mockCommand.Verify(c => c.Execute(), Moq.Times.Once))
-            {
-                throw new AssertionException("Injected command was not executed.");
-            }
+            // Assert
+            mockCommand.Verify(c => c.Execute(), Times.Once);
         }
 
+        [Test]
         public void Execute_NoInjectedCommand_ThrowsException()
         {
+            // Arrange
             var commandInjectableCommand = new CommandInjectableCommand();
 
-            try
-            {
-                commandInjectableCommand.Execute();
-                throw new AssertionException("Expected exception was not thrown.");
-            }
-            catch (InvalidOperationException)
-            {
-
-            }
-        }
-
-        private class AssertionException : Exception
-        {
-            public AssertionException(string message) : base(message) { }
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => commandInjectableCommand.Execute());
         }
     }
 }
