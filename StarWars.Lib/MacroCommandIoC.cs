4432017b4c19ebad29f06f7a5e3b16ca92d4d01a
Kreoniz <kreoniz@mail.ru>
2024-12-17 21:24:20 +0600

    feat: add macrocommand to ioc

---
+++ StarWars.Lib/MacroCommandIoC.cs
@@ -0,0 +1,12 @@
+﻿using Hwdtech;
+namespace StarWars.Lib;
+
+public class RegisterIoCDependencyMacroCommand : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.MacroCommand",
+            (Func<object, object>)(obj =>
+            new MacroCommand(IoC.Resolve<ICommand>("Adapters.MacroCommand", obj)))).Execute();
+    }
+}
