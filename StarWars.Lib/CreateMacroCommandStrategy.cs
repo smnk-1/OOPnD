1d476c689d295bba4f4d967ab017366682efc0c8
Kreoniz <kreoniz@mail.ru>
2024-12-18 13:38:08 +0600

    feat: add create macro command strategy

---
+++ StarWars.Lib/CreateMacroCommandStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Hwdtech;
+
+namespace StarWars.Lib
+{
+    public class CreateMacroCommandStrategy
+    {
+        private readonly string cmdSpec;
+
+        public CreateMacroCommandStrategy(string cmdSpec)
+        {
+            this.cmdSpec = cmdSpec;
+        }
+
+        public ICommand Resolve(object[] args)
+        {
+            var cmdsNames = IoC.Resolve<string[]>($"Specs.{cmdSpec}");
+            var cmds = cmdsNames.Select(name => IoC.Resolve<ICommand>(name, args)).ToArray();
+            return IoC.Resolve<ICommand>("Commands.Macro", cmds.Cast<object>().ToArray());
+        }
+    }
+}
--- StarWars.Lib/MacroCommandIoC.cs
+++ StarWars.Lib/MacroCommandIoC.cs
@@ -5,9 +5,9 @@ public class RegisterIoCDependencyMacroCommand : Hwdtech.ICommand
 {
     public void Execute()
     {
-            IoC.Resolve<ICommand>(
-                "IoC.Register",
-                "Commands.Macro",
-                (object[] args) => (ICommand)new MacroCommand(args.Select(x => (ICommand)x).ToArray())).Execute();
+        IoC.Resolve<ICommand>(
+            "IoC.Register",
+            "Commands.Macro",
+            (object[] args) => (ICommand)new MacroCommand(args.Select(x => (ICommand)x).ToArray())).Execute();
     }
 }
