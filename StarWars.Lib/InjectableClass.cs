0cb4056a4f0cda765936ab659e359cddcdf9841e
rabigede <108132920+rabigede@users.noreply.github.com>
2024-12-18 21:56:51 +0300

    added InjectableClass and InjectableTest

---
+++ StarWars.Lib/InjectableClass.cs
@@ -0,0 +1,25 @@
+namespace StarWars.Lib;
+
+public interface ICommandInjectable
+{
+    void Inject(ICommand command);
+    void Execute();
+}
+
+public class CommandInjectableCommand : ICommand, ICommandInjectable
+{
+    private ICommand? _injectedCommand;
+
+    public CommandInjectableCommand() { }
+
+    public void Inject(ICommand command)
+    {
+        _injectedCommand = command;
+    }
+
+    public void Execute()
+    {
+        _injectedCommand?.Execute(); 
+    }
+}
+
