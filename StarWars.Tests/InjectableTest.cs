f6ea5058d8d582969e698afa669cdfcabd64d8da
rabigede <108132920+rabigede@users.noreply.github.com>
2024-12-18 13:29:09 +0300

    Update InjectableTest.cs

--- StarWars.Tests/InjectableTest.cs
+++ StarWars.Tests/InjectableTest.cs
@@ -1,31 +1,30 @@
 ﻿using Moq;
 using StarWars.Lib;
 
-namespace StarWars.Tests
+namespace StarWars.Test;
+
+public class CommandInjectableCommandTests
 {
-    public class CommandInjectableCommandTests
+    [Fact]
+    public void Execute_InjectedCommand_ExecutesInjectedCommand()
     {
-        [Fact]
-        public void Execute_InjectedCommand_ExecutesInjectedCommand()
-        {
 
-            var mockCommand = new Mock<ICommand>();
-            mockCommand.Setup(x => x.Execute()).Verifiable();
-            var commandInjectableCommand = new CommandInjectableCommand();
-            commandInjectableCommand.Inject(mockCommand.Object);
+        var mockCommand = new Mock<ICommand>();
+        mockCommand.Setup(x => x.Execute()).Verifiable();
+        var commandInjectableCommand = new CommandInjectableCommand();
+        commandInjectableCommand.Inject(mockCommand.Object);
 
-            commandInjectableCommand.Execute();
+        commandInjectableCommand.Execute();
 
-            mockCommand.Verify();
-        }
+        mockCommand.Verify();
+    }
 
-        [Fact]
-        public void Execute_NoInjectedCommand_ThrowsException()
-        {
-            var commandInjectableCommand = new CommandInjectableCommand();
+    [Fact]
+    public void Execute_NoInjectedCommand_ThrowsException()
+    {
+        var commandInjectableCommand = new CommandInjectableCommand();
 
-            Assert.Throws<InvalidOperationException>(() => commandInjectableCommand.Execute());
-        }
+        Assert.Throws<InvalidOperationException>(() => commandInjectableCommand.Execute());
     }
 }
 
