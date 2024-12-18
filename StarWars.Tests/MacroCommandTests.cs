0267337c7967a15c1da9ade511572abfb48592d9
Kreoniz <kreoniz@mail.ru>
2024-12-18 13:28:28 +0600

    feat: add macrocommand tests

---
+++ StarWars.Tests/MacroCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using StarWars.Lib;
+
+namespace StarWars.Tests
+{
+    public class MacroCommandTests
+    {
+        [Fact]
+        public void StopOnException()
+        {
+            var cmd1 = new Mock<ICommand>();
+            var cmd2 = new Mock<ICommand>();
+            var cmd3 = new Mock<ICommand>();
+
+            cmd2.Setup(m => m.Execute()).Throws<Exception>();
+
+            var macroCommand = new MacroCommand(cmd1.Object, cmd2.Object, cmd3.Object);
+
+            Assert.Throws<Exception>(() => macroCommand.Execute());
+            cmd1.Verify(m => m.Execute(), Times.Once());
+            cmd2.Verify(m => m.Execute(), Times.Once());
+            cmd3.Verify(m => m.Execute(), Times.Never());
+        }
+    }
+}
