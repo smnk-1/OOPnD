41993bab11ab128c1d07a2e02798c841beabd2dd
rabigede <108132920+rabigede@users.noreply.github.com>
2024-12-18 03:23:07 +0300

    Update InjectableTest.cs

--- StarWars.Tests/InjectableTest.cs
+++ StarWars.Tests/InjectableTest.cs
@@ -8,26 +8,22 @@ namespace StarWars.Tests
         [Fact]
         public void Execute_InjectedCommand_ExecutesInjectedCommand()
         {
-            // Arrange
+
             var mockCommand = new Mock<ICommand>();
-            mockCommand.Setup(x => x.Execute()).Verifiable(); // Устанавливаем проверку
+            mockCommand.Setup(x => x.Execute()).Verifiable();
             var commandInjectableCommand = new CommandInjectableCommand();
             commandInjectableCommand.Inject(mockCommand.Object);
 
-            // Act
             commandInjectableCommand.Execute();
 
-            // Assert
-            mockCommand.Verify(); // Проверяем, что Execute() был вызван
+            mockCommand.Verify();
         }
 
         [Fact]
         public void Execute_NoInjectedCommand_ThrowsException()
         {
-            // Arrange
             var commandInjectableCommand = new CommandInjectableCommand();
 
-            // Act & Assert
             Assert.Throws<InvalidOperationException>(() => commandInjectableCommand.Execute());
         }
     }
