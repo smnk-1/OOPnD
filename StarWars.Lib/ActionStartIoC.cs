b88187821a286ba0eed0958caa6e298faef8c951
smnk-1 <semikinnikolay1@gmail.com>
2024-12-24 00:09:03 +0600

    feat: add Actions.Start

---
+++ StarWars.Lib/ActionStartIoC.cs
@@ -0,0 +1,27 @@
+﻿using Hwdtech;
+
+namespace StarWars.Lib;
+
+public class RegisterIoCDependencyActionsStart : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Actions.Start", (object[] args) =>
+        {
+            if (args.Length != 1 || args[0] is not IDictionary<string, object> order)
+            {
+                throw new ArgumentException("Expected a single argument of type IDictionary<string, object>.");
+            }
+
+            var commandNames = order.ContainsKey("Commands") && order["Commands"] is IEnumerable<string> commands
+                ? commands
+                : throw new InvalidOperationException("Order must contain a 'Commands' key with a list of command names.");
+
+            var commandsArray = commandNames
+                .Select(name => IoC.Resolve<StarWars.Lib.ICommand>(name))
+                .ToArray();
+
+            return new MacroCommand(commandsArray);
+        }).Execute();
+    }
+}
---
+++ StarWars.Tests/ActionStartIoCTests.cs
@@ -0,0 +1,42 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using StarWars.Lib;
+
+public class RegisterIoCDependencyActionsStartTests
+{
+    public RegisterIoCDependencyActionsStartTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+    }
+
+    [Fact]
+    public void Execute_ShouldRegisterActionsStartDependency()
+    {
+
+        var iocScope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", iocScope).Execute();
+
+        var command_1 = new Mock<StarWars.Lib.ICommand>();
+        var command_2 = new Mock<StarWars.Lib.ICommand>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Command1", (object[] args) => command_1.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Command2", (object[] args) => command_2.Object).Execute();
+
+        var order = new Dictionary<string, object>
+        {
+            { "Commands", new[] { "Command1", "Command2" } }
+        };
+
+        new RegisterIoCDependencyActionsStart().Execute();
+
+        var macroCommand = IoC.Resolve<StarWars.Lib.ICommand>("Actions.Start", order);
+
+        Assert.NotNull(macroCommand);
+        Assert.IsType<MacroCommand>(macroCommand);
+
+        macroCommand.Execute();
+        command_1.Verify(c => c.Execute(), Times.Once());
+        command_2.Verify(c => c.Execute(), Times.Once());
+    }
+}
