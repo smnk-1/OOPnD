57d200c2b9b403234d7e41543d9b03e7909b2a9f
smalx114 <alx444bp@mail.ru>
2024-12-26 01:36:31 +0600

    fix: test

--- StarWars.Tests/SendCommandIoCTests.cs
+++ StarWars.Tests/SendCommandIoCTests.cs
@@ -20,14 +20,7 @@ public class RegisterIoCDependencySendCommandTests
         var mockCommand = new Mock<StarWars.Lib.ICommand>();
         var mockReceiver = new Mock<ICommandReceiver>();
 
-        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.Send",
-            (Func<object[], object>)(obj =>
-            {
-                return new SendCommand(
-                    (StarWars.Lib.ICommand)obj[0],
-                    (ICommandReceiver)obj[1]
-                );
-            })).Execute();
+        new RegisterIoCDependencySendCommand().Execute();
 
         var sendCommand = IoC.Resolve<StarWars.Lib.ICommand>("Commands.Send", new object[] { mockCommand.Object, mockReceiver.Object });
 
