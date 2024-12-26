611694e830d8bedc0feab4d8d2192410394f52f8
smnk-1 <semikinnikolay1@gmail.com>
2024-12-26 16:17:02 +0600

    chore: copied MacroCommand

---
+++ StarWars.Lib/MacroCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace StarWars.Lib;
+
+public class MacroCommand : Hwdtech.ICommand
+{
+    public readonly IEnumerable<Hwdtech.ICommand> cmds;
+
+    public MacroCommand(IEnumerable<Hwdtech.ICommand> commands)
+    {
+        cmds = commands;
+    }
+
+    public void Execute()
+    {
+        var commandList = cmds.ToList();
+        commandList.ForEach(c => c.Execute());
+    }
+}
