9de2029faeb5975e1191a340aabc0b3dce7ed6c4
smnk-1 <semikinnikolay1@gmail.com>
2024-12-26 15:01:14 +0600

    ActionStop register and tests

---
+++ StarWars.Lib/ActionStop.cs
@@ -0,0 +1,18 @@
+﻿using Hwdtech;
+
+using StarWars.Lib;
+
+public class RegisterIoCDependencyActionsStop : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Actions.Stop", (object[] args) =>
+        {
+            var order = (IDictionary<string, object>)args[0];
+
+            return new StopCommand(
+                (IDictionary<string, object>)order["gameObject"],
+                (string)order["commandType"]);
+        }).Execute();
+    }
+}
---
+++ StarWars.Tests/ActionStopTest.cs
@@ -0,0 +1,29 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using StarWars.Lib;
+
+namespace StarWars.Tests;
+
+public class RegisterIoCDependencyActionsStopTests
+{
+    public RegisterIoCDependencyActionsStopTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        var iocScope = IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"));
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", iocScope).Execute();
+    }
+
+    [Fact]
+    public void ExecuteShouldResolveRegisterStopCommand()
+    {
+        var order = new Dictionary<string, object>();
+        order["gameObject"] = new Dictionary<string, object>();
+        order["commandType"] = "Type";
+
+        new RegisterIoCDependencyActionsStop().Execute();
+        var stopCommand = IoC.Resolve<Hwdtech.ICommand>("Actions.Stop", order);
+
+        Assert.NotNull(stopCommand);
+        Assert.IsType<StopCommand>(stopCommand);
+    }
+}
