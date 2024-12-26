0b9ef1753fcba0459081191c995dbc31fd31cfd7
Kreoniz <kreoniz@mail.ru>
2024-12-26 09:48:52 +0600

    fix: test coverage

--- StarWars.Tests/CreateMacroCommandStrategyTests.cs
+++ StarWars.Tests/CreateMacroCommandStrategyTests.cs
@@ -22,16 +22,19 @@ namespace StarWars.Tests
                 (Func<object[], object>)((args) => new[] { "Command1", "Command2" })
             ).Execute();
 
+            var command1Mock = new Mock<Hwdtech.ICommand>();
+            var command2Mock = new Mock<Hwdtech.ICommand>();
+
             IoC.Resolve<Hwdtech.ICommand>(
                 "IoC.Register",
                 "Command1",
-                (Func<object[], object>)((args) => new Mock<Hwdtech.ICommand>().Object)
+                (Func<object[], object>)((args) => command1Mock.Object)
             ).Execute();
 
             IoC.Resolve<Hwdtech.ICommand>(
                 "IoC.Register",
                 "Command2",
-                (Func<object[], object>)((args) => new Mock<Hwdtech.ICommand>().Object)
+                (Func<object[], object>)((args) => command2Mock.Object)
             ).Execute();
 
             var strategy = new CreateMacroCommandStrategy("Test");
@@ -39,11 +42,42 @@ namespace StarWars.Tests
             var macroCommand = strategy.Resolve(Array.Empty<object>());
             macroCommand.Execute();
 
-            var command1Mock = IoC.Resolve<Hwdtech.ICommand>("Command1") as Mock<Hwdtech.ICommand>;
-            var command2Mock = IoC.Resolve<Hwdtech.ICommand>("Command2") as Mock<Hwdtech.ICommand>;
+            command1Mock.Verify(m => m.Execute(), Times.Once());
+            command2Mock.Verify(m => m.Execute(), Times.Once());
+        }
 
-            command1Mock?.Verify(m => m.Execute(), Times.Once());
-            command2Mock?.Verify(m => m.Execute(), Times.Once());
+        [Fact]
+        public void ResolveMacroCommand_EmptyCommandList()
+        {
+            IoC.Resolve<Hwdtech.ICommand>(
+                "IoC.Register",
+                "Specs.Empty",
+                (Func<object[], object>)((args) => Array.Empty<string>())
+            ).Execute();
+
+            var strategy = new CreateMacroCommandStrategy("Empty");
+
+            var macroCommand = strategy.Resolve(Array.Empty<object>());
+
+            var exception = Record.Exception(() => macroCommand.Execute());
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ResolveMacroCommand_NullCommandList()
+        {
+            IoC.Resolve<Hwdtech.ICommand>(
+                "IoC.Register",
+                "Specs.Null",
+                (Func<object[], object>)((args) => null!)
+            ).Execute();
+
+            var strategy = new CreateMacroCommandStrategy("Null");
+
+            var macroCommand = strategy.Resolve(Array.Empty<object>());
+
+            var exception = Record.Exception(() => macroCommand.Execute());
+            Assert.Null(exception);
         }
     }
 }
