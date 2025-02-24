1cafeef599db30bf1eec625b207dc79c18eaceb1
smnk-1 <semikinnikolay1@gmail.com>
2025-02-24 18:46:14 +0600

    feat: Game Items Repository and tests

---
+++ StarWars.Lib/GameItemRepository.cs
@@ -0,0 +1,15 @@
+using Hwdtech;
+
+namespace StarWars.Lib;
+
+public class GameRepository: IRepository{
+    public IDictionary<string, object> GetItem(object id){
+        return IoC.Resolve<IDictionary<string, object>>("GameItem", id);
+    }
+    public void AddItem(object id, IDictionary<string, object> GameItem){
+        IoC.Resolve<object>("GameItem.Add", id, GameItem);
+    }
+    public void RemoveItem(object id){
+        IoC.Resolve<IDictionary<string, object>>("GameItem.Remove", id);
+    }
+}
---
+++ StarWars.Tests/GameItemRepositoryTests.cs
@@ -0,0 +1,35 @@
+using Hwdtech;
+using Hwdtech.Ioc;
+using StarWars.Lib;
+
+namespace StarWars.Tests;
+
+public class GameItemRepositoryTests{
+    public GameItemRepositoryTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        new RegisterDependenciesGameItem().Execute();
+    }
+
+    [Fact]
+    public void GameRepositoryTests(){
+        var GameObject = new Dictionary<string, object>(){
+            {"key", "value"}
+        };
+        var gameRepo = new GameRepository();
+
+        gameRepo.AddItem("spaceship01", GameObject);
+
+        var result = gameRepo.GetItem("spaceship01");
+
+        Assert.Equal(GameObject, result);
+
+        gameRepo.RemoveItem("spaceship01");
+
+        Assert.Throws<KeyNotFoundException>(() => gameRepo.GetItem("spaceship01"));
+    }
+}
