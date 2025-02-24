14b862ea0190d1f0d0336710ddce1318bb941c9b
smnk-1 <semikinnikolay1@gmail.com>
2025-02-24 18:45:29 +0600

    fix: change IoC lambda-functions

--- StarWars.Lib/RegisterDependenciesGameItem.cs
+++ StarWars.Lib/RegisterDependenciesGameItem.cs
@@ -6,24 +6,34 @@ public class RegisterDependenciesGameItem: Hwdtech.ICommand
 {
     public void Execute()
     {
-        var gameItems = new Dictionary<string, Dictionary<string, object>>();
+        var gameItems = new Dictionary<string, IDictionary<string, object>>();
 
-        IoC.Resolve<ICommand>(
+        IoC.Resolve<Hwdtech.ICommand>(
             "IoC.Register", 
             "GameItem", 
             (object[] args) => gameItems[(string)args[0]]
         ).Execute();
 
-        IoC.Resolve<ICommand>(
+        IoC.Resolve<Hwdtech.ICommand>(
             "IoC.Register", 
             "GameItem.Add",
-            (object[] args) => gameItems.Add((string)args[0], (Dictionary<string, object>) args[1])
+            (Func<object[], object>)(args =>
+            {
+                gameItems.Add((string)args[0], (IDictionary<string, object>)args[1]);
+                return null;
+            })
         ).Execute();
 
-        IoC.Resolve<ICommand>(
+        IoC.Resolve<Hwdtech.ICommand>(
             "IoC.Register", 
             "GameItem.Remove",
-            (object[] args) => gameItems.Remove((string)args[0])
+            (Func<object[], object>)(args =>
+            {
+                gameItems.Remove((string)args[0]);
+                return null;
+            })
         ).Execute();
+
+
     }
 }
