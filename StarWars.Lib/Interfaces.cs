cfb55744b7e4c81cde4b4ebd0558954aad1cdb5d
smnk-1 <semikinnikolay1@gmail.com>
2025-02-24 14:09:03 +0600

    feat: create IRepository

--- StarWars.Lib/Interfaces.cs
+++ StarWars.Lib/Interfaces.cs
@@ -9,3 +9,10 @@ public interface ICommandReceiver
 {
     void Receive(Hwdtech.ICommand cmd);
 }
+
+public interface IRepository
+{
+    object GetItem(object id);
+    void AddItem(object id, object GameItem);
+    void RemoveItem(object id);
+}
