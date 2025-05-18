dcc5ac3e709e3555d07028b06381b8103b26e406
smnk-mcb <semikinnikolay1@gmail.com>
2025-05-18 14:49:43 +0600

    feat: Adatpters.Generate registration

---
+++ StarWars.Lib/GenerateAdaptersIoc.cs
@@ -0,0 +1,46 @@
+using Hwdtech;
+namespace StarWars.Lib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+public class RegisterIoCDependencyAdaptersGenerate : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<object>("IoC.Register", "Adapters.Generate",
+        (Func<object[], object>)(args => 
+        {
+            var adaptee = args[0];
+            var targetType = (Type)args[1];
+
+            if (targetType.IsInstanceOfType(adaptee))
+            {
+                return adaptee;
+            }
+
+            var adapterKey = $"{adaptee.GetType().FullName}->{targetType.FullName}";
+            var adapters = IoC.Resolve<IDictionary<string, IAdapter>>("Adapters.Dictionary");
+
+            if (adapters.TryGetValue(adapterKey, out var existingAdapter))
+            {
+                return existingAdapter.Adapt(adaptee);
+            }
+
+            var template = IoC.Resolve<string>("Adapters.GetTemplate", targetType);
+            var sourceCode = IoC.Resolve<string>("Adapters.GenerateAdapterCode", adaptee.GetType(), targetType, template);
+            var assembly = IoC.Resolve<Assembly>("Adapters.Compile", adaptee.GetType().Assembly, sourceCode);
+
+            var adapterType = assembly.GetTypes()
+                .First(t => targetType.IsAssignableFrom(t));
+            
+            var adapter = (IAdapter)Activator.CreateInstance(adapterType);
+            
+            adapters[adapterKey] = adapter;
+            
+            return adapter.Adapt(adaptee);
+
+        }));
+    }
+}
